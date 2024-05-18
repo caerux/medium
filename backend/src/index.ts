342d09b9042bebc7b1d033ea4bf184ea16b1895c
caerux <ak.ankitkumar0202@gmail.com>
2024-05-18 22:11:58 +0530

    medium backend

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign, verify } from "hono/jwt";
+import { userRouter } from "./router/user";
+import { blogRouter } from "./router/blog";
+
+// Create the main Hono app
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
+
+app.use("/api/v1/blog/*", async (c, next) => {
+  const header = c.req.header("authorization") || "";
+  const token = header.split(" ")[1];
+  const response = await verify(token, c.env.JWT_SECRET);
+  if (response.id) {
+    next();
+  } else {
+    c.status(403);
+    return c.json({ error: "unauthorized" });
+  }
+});
+
+export default app;
